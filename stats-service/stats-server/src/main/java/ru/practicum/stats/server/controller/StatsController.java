0dc4a2b712e0ecf0480ccddf8e008d8ff1b96e11
badsoul19 <treeworld05@gmail.com>
2026-03-03 20:26:26 +0300

    Реализация основного сервиса, исправить код для postman тестов

--- stats-service/stats-server/src/main/java/ru/practicum/stats/server/controller/StatsController.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/server/controller/StatsController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.practicum.stats.dto.EndpointHit;
 import ru.practicum.stats.dto.ViewStats;
 import ru.practicum.stats.server.service.StatsService;
@@ -28,6 +29,9 @@ public class StatsController {
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
             @RequestParam(required = false) List<String> uris,
             @RequestParam(defaultValue = "false") boolean unique) {
+        if (start.isAfter(end)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Start must be before end");
+        }
         return statsService.getStats(start, end, uris, unique);
     }
 }
